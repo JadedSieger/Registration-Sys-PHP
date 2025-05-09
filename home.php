a9db5247267509510e4cc9b5ebbe8df8d1542608
Simpcast Productions <66287897+JadedSieger@users.noreply.github.com>
2025-05-09 16:48:42 +0800

    Update home.php

--- home.php
+++ home.php
@@ -63,8 +63,8 @@ if (!isset($_SESSION['id'])) {
                 <h2>NERV Operations</h2>
                 <p>Here you will find details on the various aspects of NERV, from the development of the Evangelions to our top-secret projects.</p>
                 <ul>
-                    <li><a href="#">Evangelion Units</a></li>
-                    <li><a href="#">NERV Facilities</a></li>
+                    <li><a href="/units.php">Evangelion Units</a></li>
+                    <li><a href="#/facilities.php">NERV Facilities</a></li>
                     <li><a href="#">Project Yashima</a></li>
                     <li><a href="#">Operation Exodus</a></li>
                 </ul>
