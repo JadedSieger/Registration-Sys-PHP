482b1fe3b73be6a59cff84a486d1282fc4fb34b3
Simpcast Productions <66287897+JadedSieger@users.noreply.github.com>
2025-05-10 16:26:26 +0800

    Update about.php

--- about.php
+++ about.php
@@ -44,7 +44,6 @@
             </div>
 
             <div class="lore-video">
-                <h2>Learn About NERV</h2>
                 <iframe width="560" height="315" src="https://www.youtube.com/embed/PBRqncfrYtI?si=FwPkFZ3vrBMByrPh" title="YouTube video player" frameborder="0" allow="accelerometer;
                   autoplay; clipboard-write;
                   encrypted-media;
