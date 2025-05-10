d139515c36ff683a423cd535d5815e31b131133e
Simpcast Productions <66287897+JadedSieger@users.noreply.github.com>
2025-05-10 17:00:07 +0800

    Update session.php

--- session.php
+++ session.php
@@ -2,7 +2,7 @@
 session_start();
 
 $is_logged_in = isset($_SESSION['id']);
-
+$first_name = "" ;
 $first_name = ($is_logged_in && isset($_SESSION['first_name']))
     ? $_SESSION['first_name']
     : 'Guest';
